2323b6ae7d0c85ea858f53db4b7713f7e08d3606
hazimrzk <azizul.hazim.00@gmail.com>
2025-10-11 13:21:16 +0800

    Create Project
    Signed-off-by: hazimrzk <azizul.hazim.00@gmail.com>

---
+++ Movies/Movies/MoviesApp.swift
@@ -0,0 +1,17 @@
+//
+//  MoviesApp.swift
+//  Movies
+//
+//  Created by Hazim Razak on 11/10/2025.
+//
+
+import SwiftUI
+
+@main
+struct MoviesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
